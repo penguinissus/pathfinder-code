a5295cac79f215ac60517a773bf62881a83944d1
penguinissus <jocelyn.ty.lau@gmail.com>
2025-07-29 16:20:25 -0400

    flickerLight function

--- pathfinder.ino
+++ pathfinder.ino
@@ -32,8 +32,29 @@ void loop() {
 }
 
 void flickerLight() {
+    digitalWrite(LED1, LOW);
+    digitalWrite(LED2, LOW);
+    digitalWrite(LED3, LOW);
     for(int i = 0; i < 12; i++){
-        switch (i%4) //to be completed
+        switch (i%4) {
+            case 0:
+                digitalWrite(LED1, HIGH);
+                digitalWrite(LED4, LOW);
+                break;
+            case 1:
+                digitalWrite(LED2, HIGH);
+                digitalWrite(LED1, LOW);
+                break;
+            case 2:
+                digitalWrite(LED3, HIGH);
+                digitalWrite(LED2, LOW);
+                break;
+            case 3:
+                digitalWrite(LED4, HIGH);
+                digitalWrite(LED3, LOW);
+                break;
+            //do i need an else to finish switch case? research    
+        }
         delay(100);
     }
 }
